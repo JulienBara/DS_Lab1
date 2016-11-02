f9c2206dac6b9030168e1b0000e4f63fabd29b99
JulienBara <bara.julien@yahoo.fr>
2016-11-02 18:33:30 +0000

    initial commit

---
+++ client/client.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+
+host = "localhost"
+port = 8000
+
+while 1:
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    try :
+        socket.connect((host, port))
+        print 'Connected'
+    except :
+        print 'Connection error'
+        sys.exit()
+
+    msg = input("What is your message? (X to quit): ")
+
+    if msg == 'X' or msg == 'x':
+        socket.close()
+        break
+
+    else:
+        socket.send("GET /?message=" + msg + "\n\n")
+        print socket.recv(4096)
